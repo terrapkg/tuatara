2e56a2d841cc9200660a8f501c15947e48a1b3c4
madonuko <madonuko@outlook.com>
2026-07-20 18:38:00 +0800

    fix(ci): madoguchi

--- .github/workflows/mg.sh
+++ .github/workflows/mg.sh
@@ -6,7 +6,7 @@ export p="{\"id\":\"$5\",\"ver\":\"%v\",\"rel\":\"%r\",\"arch\":\"$4\",\"dirs\":
 if [[ $1 == false ]]; then
 	d=${p/\%v/?}
 	d=${d/\%r/?}
-	curl -H "Authorization: Bearer $6" https://madoguchi.fyralabs.com/ci5/terra$3/builds/f -X PUT -H "Content-Type: application/json" -d "$d" --fail-with-body
+	curl -H "Authorization: Bearer $6" https://madoguchi.fyralabs.com/ci5/tuatara-$3/builds/f -X PUT -H "Content-Type: application/json" -d "$d" --fail-with-body
 	exit 0
 fi
 
@@ -16,5 +16,5 @@ for f in anda-build/rpm/rpms/*; do
 	r=$(lesspipe.sh $f | grep -E "Release\s*: " | sed "s@Release\s*: @@" | head -n1)
 	d=${p/\%v/$v}
 	d=${d/\%r/$r}
-	curl -H "Authorization: Bearer $6" https://madoguchi.fyralabs.com/ci5/terra$3/builds/$n -X PUT -H "Content-Type: application/json" -d $d --fail-with-body
+	curl -H "Authorization: Bearer $6" https://madoguchi.fyralabs.com/ci5/tuatara-$3/builds/$n -X PUT -H "Content-Type: application/json" -d $d --fail-with-body
 done
